de7af8f2bedc76614af41f41e8bdcbf0eeb9414c
Maykel Santos <maykelsantoz@gmail.com>
2020-07-18 19:30:31 -0300

    Add configuration for API_CORS

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const options:cors.CorsOptions = {
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
   credentials: true,
   methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-  origin: process.env.API_URL,
+  origin: process.env.API_CORS,
   preflightContinue: false,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
