52f870aaab5a2ecbc7f646b99db4d0ce9220fd33
Maykel Santos <maykelsantoz@gmail.com>
2020-07-18 12:20:16 -0300

    New configuration to database Postgress URL

--- knexfile.ts
+++ knexfile.ts
@@ -3,13 +3,7 @@ import 'dotenv/config'
 
 module.exports = {
   client: 'pg',
-  connection: {
-    host: String(process.env.DB_HOST),
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
+  connection: process.env.DATABASE_URL,
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
 
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -12,13 +12,7 @@ import 'dotenv/config'
 
 const connection = knex({
   client: 'pg',
-  connection: {
-    host: String(process.env.DB_HOST),
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  }
+  connection: process.env.DATABASE_URL
 })
 
 export default connection;
