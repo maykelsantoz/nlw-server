9e690d2856a0860478dcb8f4cd690256fbb3a4f0
Maykel Santos <maykelsantoz@gmail.com>
2020-07-18 17:02:08 -0300

    New configuration

---
+++ dist/knexfile.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var path_1 = __importDefault(require("path"));
+require("dotenv/config");
+module.exports = {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        directory: path_1.default.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    seeds: {
+        directory: path_1.default.resolve(__dirname, 'src', 'database', 'seeds')
+    }
+};
