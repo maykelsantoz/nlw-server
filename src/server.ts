aca1bbeff52b3259a43877927425722c564b7c62
unknown <supri@DESKTOP-8NRHKUN.ldb.org>
2020-07-20 13:49:32 -0300

    New config for Database DB

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,49 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import routes from './routes';
+import express from "express";
+import cors from "cors";
+import path from "path";
+import routes from "./routes";
 
-import { errors } from 'celebrate';
-require('dotenv').config();
+import { errors } from "celebrate";
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors({credentials:true, origin: process.env.FRONTEND || 'http://localhost:3333'}));
+app.use(function (req, res, next) {
+  var oneof = false;
+  if (req.headers.Origin) {
+    res.header("Access-Control-Allow-Origin", req.headers.Origin);
+    oneof = true;
+  }
+  if (req.headers["access-control-request-method"]) {
+    res.header(
+      "Access-Control-Allow-Methods",
+      req.headers["access-control-request-method"]
+    );
+    oneof = true;
+  }
+  if (req.headers["access-control-request-headers"]) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      req.headers["access-control-request-headers"]
+    );
+    oneof = true;
+  }
+  if (oneof) {
+    res.header("Access-Control-Max-Age", String(60 * 60 * 24 * 365));
+  }
+
+  // intercept OPTIONS method
+  if (oneof && req.method == "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 
 // app.use(cors(), (req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   res.header(
-//     'Access-Control-Allow-Headers', 
+//     'Access-Control-Allow-Headers',
 //     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 //     );
 //     if (req.method === 'OPTIONS') {
@@ -28,10 +58,10 @@ app.use(routes);
 
 //app.options('*', cors(options));
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.use(errors());
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log('⚡ Server start')
+  console.log("⚡ Server start");
 });
