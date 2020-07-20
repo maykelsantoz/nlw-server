15684930002093833609eb27653d3b4997313ad6
Maykel Santos <maykelsantoz@gmail.com>
2020-07-19 22:47:41 -0300

    Configuration CORS with origin and DOTENV

--- knexfile.ts
+++ knexfile.ts
@@ -5,34 +5,26 @@ module.exports = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_DEV,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations')
 
     },
     seeds: {
       directory: path.resolve(__dirname, 'src', 'database', 'seeds')
-    },
-    useNullAsDefault: true,
+    }
+    //useNullAsDefault: true,
   },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations')
 
     },
     seeds: {
       directory: path.resolve(__dirname, 'src', 'database', 'seeds')
-    },
-    useNullAsDefault: true,
+    }
+    //useNullAsDefault: true,
   }
 }
