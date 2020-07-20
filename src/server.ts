0b72d8aa1652d9ddb3d698f7379fe9e3597ba673
Maykel Santos <maykelsantoz@gmail.com>
2020-07-19 21:18:58 -0300

    Configuration CORS with origin and DOTENV

--- src/server.ts
+++ src/server.ts
@@ -8,18 +8,20 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors(), (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers", 
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === 'OPTIONS') {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-  next();
-});
+app.use(cors({credentials:true, origin: process.env.FRONTEND || 'http://localhost:3000'}));
+
+// app.use(cors(), (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers", 
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+//     if (req.method === 'OPTIONS') {
+//       res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+//       return res.status(200).json({});
+//     }
+//   next();
+// });
 
 app.use(express.json());
 app.use(routes);
