d6e161c6b8568f14208a7655bc2a53727088d90d
unknown <supri@DESKTOP-8NRHKUN.ldb.org>
2020-07-23 12:58:27 -0300

    New config for CORS OLD

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,27 @@ require("dotenv").config();
 
 const app = express();
 
-var corsOptions = {
-  origin: "https://nlw01-web.herokuapp.com/create-point",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+fetch("http://nlw01-web.herokuapp.com/create-point", {
+  headers: {
+    accepts: "application/json",
+  },
+})
+  .then((res) => {
+    console.log(res);
+    return res.json();
+  })
+  .then((json) => console.log(json))
+  .catch((a) => {
+    console.log(a);
+  });
+
+// var corsOptions = {
+//   origin: process.env.FRONTEND,
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(routes);
 
 //app.options('*', cors(options));
